60d4502d85c11e90e6fc956594d797cdb97f513e
Ryan Singh <ryansingh@ymail.com>
2022-05-10 16:10:56 +0100

    Added Jokes

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+jokes = [
+    "Whats the object-oriented way to become wealthy? Inheritance",
+    "!false - It's funny 'cause it's true.",
+    "Why did the functions stop calling each other? Because they had constant arguments."
+]
+
 
 @app.get("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def tell_a_joke():
+    joke = random.choice(jokes)
+    return f"<p>{joke}</p>"
